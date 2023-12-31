fbbe4c7b3a90a6cffd43bc27a52ec7445a7ef9b3
Abyssun <2025353414@qq.com>
2023-12-31 18:02:10 +0800

    update

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class LoginServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        System.out.println(12);
     }
 
     @Override
